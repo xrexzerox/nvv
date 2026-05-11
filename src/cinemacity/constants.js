74d093b76c2f933bf1c374ddfe133bc7620db77a
Rey Omayan <30548067+xrexzerox@users.noreply.github.com>
2026-05-12 07:06:07 +0800

    Update cookie credentials in constants.js

--- src/cinemacity/constants.js
+++ src/cinemacity/constants.js
@@ -1,7 +1,7 @@
 export const MAIN_URL = "https://cinemacity.cc";
 export const HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/120.0.0.0 Safari/537.36",
-    "Cookie": "dle_user_id=32729; dle_password=894171c6a8dab18ee594d5c652009a35;",
+    "Cookie": "dle_user_id=56816; dle_password=7a92842625c17b43c2605440b26dc603;",
     "Referer": "https://cinemacity.cc/"
 };
 export const TMDB_API_KEY = "1865f43a0549ca50d341dd9ab8b29f49";
